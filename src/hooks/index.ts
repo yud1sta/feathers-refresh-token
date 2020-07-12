9ca89f7715de82db952541495f31b7b0a624d4b8
Jacky Wu <jackywxd@gmail.com>
2020-07-11 20:54:20 -0700

    Update package and include missing exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,4 @@
-import Debug from 'debug';
-const debug = Debug('feathers-refresh-token');
-
 export * from './issue-refresh-token';
 export * from './refresh-access-token';
 export * from './logout-users';
+export * from './common';
